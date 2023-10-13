2e706a4b6233d70c3c1a7883e922abaf49ff52e9
DommSite <johan.johansson06@live.se>
2023-10-13 13:13:31 +0200

    Add project files.

---
+++ Programmeringsövning4.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace programmeringsovning4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int årtal = 1495; årtal>=1400;årtal =årtal - 5)
+            {
+                Console.WriteLine(årtal);
+            }
+        }
+    }
+}
